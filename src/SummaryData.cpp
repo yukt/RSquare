b500b86350c962076511107eaf16a807b1d56a76
Ketian Yu <yukt@Ketians-iMac.ddns.sph.umich.edu>
2017-11-01 14:14:58 -0400

    clean the code

--- src/SummaryData.cpp
+++ src/SummaryData.cpp
@@ -6,15 +6,10 @@
 
 bool SummaryData::read()
 {
-    const int       MAXNUM = 10000000;
-//    const int       MAXNUM_SAME_POS = 100;
+//    const int       MAXNUM = 10000000;
     VcfFileReader   inFileV, inFileI;
     VcfHeader       headerV, headerI;
     VcfRecord       recordV, recordI;
-//    int             chrV, chrI, posV, posI;
-
-//    vector<String>        TempAltV;
-//    vector<SummaryRecord> TempRecordV;
 
     inFileV.open(FileNameValidation, headerV);
     inFileI.open(FileNameImputation, headerI);
@@ -28,7 +23,12 @@ bool SummaryData::read()
     cout << "Reading VCFfiles ..." << endl;
     numSamples = headerV.getNumSamples();
 
-    SumDat.resize(MAXNUM); CHROM.resize(MAXNUM); POS.resize(MAXNUM); REF.resize(MAXNUM); ALT.resize(MAXNUM);
+    unsigned long maxNum = 0;
+    while (inFileI.readRecord(recordI)) { maxNum++; }
+    inFileI.close();
+    inFileI.open(FileNameImputation, headerI);
+
+    SumDat.resize(maxNum); CHROM.resize(maxNum); POS.resize(maxNum); REF.resize(maxNum); ALT.resize(maxNum);
     numRecords = 0;
 
     while (inFileV.readRecord(recordV))
@@ -59,9 +59,6 @@ bool SummaryData::read()
 
         for (int i = 0; i < numSamples; i++)
         {
-//            cout << info2ds(GenotypeV,"GT",i) << "\t" << *GenotypeV.getString("GT",i) << "\t" << *GenotypeV.getString("DS",i) << endl;
-//            double X = stod(*GenotypeV.getString("DS",i));
-//            double Y = stod(*GenotypeI.getString("DS",i));
             double X = info2ds(GenotypeV, validationFormat, i);
             double Y = info2ds(GenotypeI, imputationFormat, i);
             temp[0] += X; temp[1] += Y; temp[2] += X*Y; temp[3] += X*X; temp[4] += Y*Y;
@@ -74,149 +71,6 @@ bool SummaryData::read()
     cout << "Finish reading " << numRecords << " Common Records." << endl;
 
     SumDat.resize(numRecords); CHROM.resize(numRecords); POS.resize(numRecords); REF.resize(numRecords); ALT.resize(numRecords);
-
-//    bool EndRecord = !(inFileV.readRecord(recordV) & inFileI.readRecord(recordI));
-//    chrV = chr2int(recordV.getChromStr()); chrI = chr2int(recordI.getChromStr());
-//
-//
-//    while (!EndRecord)
-//    {
-//        // read until CHROM of V and I are the same.
-//        bool ChrCheck = (chrV == chrI);
-//        while (!EndRecord and !ChrCheck)
-//        {
-//            while (chrV < chrI) { if(!(inFileV.readRecord(recordV))) {EndRecord = true; break;} chrV = chr2int(recordV.getChromStr());}
-//            while (chrV > chrI) { if(!(inFileI.readRecord(recordI))) {EndRecord = true; break;} chrI = chr2int(recordI.getChromStr());}
-//            if (chrV == chrI) { ChrCheck = true; }
-//        }
-//        if ( !ChrCheck ){ break; }
-//
-//        // Under the same CHROM, read until POS of V and I are the same.
-//        int currentChr = chrV;
-//        posV = recordV.get1BasedPosition(); posI = recordI.get1BasedPosition();
-//        bool PosCheck = (posV == posI);
-//        while (!PosCheck & ChrCheck & !EndRecord)
-//        {
-//            while(posV < posI)
-//            {
-//                if(!(inFileV.readRecord(recordV)))     { EndRecord = true; break; }
-//                chrV = chr2int(recordV.getChromStr()); if(chrV > currentChr) { ChrCheck = false; break; }
-//                posV = recordV.get1BasedPosition();
-//            }
-//            while(posV > posI)
-//            {
-//                if(!(inFileI.readRecord(recordI)))     { EndRecord = true; break; }
-//                chrI = chr2int(recordI.getChromStr()); if(chrI > currentChr) { ChrCheck = false; break; }
-//                posI = recordI.get1BasedPosition();
-//            }
-//            if (ChrCheck and posV == posI) {PosCheck = true;}
-//        }
-//
-//        if (!ChrCheck) { continue; }
-//        if (!PosCheck) { break;    }
-//
-//        // start to compare ALT
-//        String altV = recordV.getAltStr(), altI = recordI.getAltStr();
-//
-//        if (altV == altI)
-//        {
-////            cout << numRecords << " " << chrV << " " << posV << " " << altV << endl;
-//
-//            markerID[numRecords] = recordI.getIDStr();
-//            REF[numRecords]      = recordI.getRefStr();
-//            ALT[numRecords]      = recordI.getAltStr();
-//            SumDat[numRecords].resize(5,0);
-//            // [0]sumX [1]sumY [2]sumXY [3]sumX2 [4]sumY2
-//
-//            VcfRecordGenotype& GenotypeV = recordV.getGenotypeInfo();
-//            VcfRecordGenotype& GenotypeI = recordI.getGenotypeInfo();
-//            vector<double> &temp = SumDat[numRecords];
-//
-//            for (int i = 0; i < numSamples; i++)
-//            {
-//                double X = stod(*GenotypeV.getString("DS",i));
-//                double Y = stod(*GenotypeI.getString("DS",i));
-//                temp[0] += X; temp[1] += Y; temp[2] += X*Y; temp[3] += X*X; temp[4] += Y*Y;
-//            }
-//
-//            numRecords++;
-//
-//
-//            EndRecord = !(inFileV.readRecord(recordV) and inFileI.readRecord(recordI));
-//            chrV = chr2int(recordV.getChromStr()); chrI = chr2int(recordI.getChromStr());
-//            continue;
-//        }
-//
-//        else
-//        {
-//            TempAltV.clear();       TempAltV.resize(MAXNUM_SAME_POS);       TempAltV[0]    = altV;
-//            TempRecordV.clear();    TempRecordV.resize(MAXNUM_SAME_POS);    TempRecordV[0] = SummaryRecord(&recordV);
-//            int numTempV   = 1;
-//            int currentPos = posV;
-//
-//            // read fileV until the next position or end of the fileV.
-//            while (true)
-//            {
-//                if (!inFileV.readRecord(recordV)) { EndRecord = true; break; }
-//                chrV = chr2int(recordV.getChromStr()); posV = recordV.get1BasedPosition();
-//                if (posV != currentPos or chrV != currentChr) { break; }
-//                TempAltV[numTempV] = recordV.getAltStr();
-//                TempRecordV[numTempV] = SummaryRecord(&recordV);
-//                numTempV++;
-//            }
-//
-//            // read fileI and compare ALT until the next POS or END.
-//            while (true)
-//            {
-//                for (int j = 0; j < numTempV; j++)
-//                {
-//                    if (altI == TempAltV[j])
-//                    {
-////                        cout << numRecords << " " << TempRecordV[j].ChromStr << " " << TempRecordV[j].BasedPosition <<  " " << altI << endl;
-//
-//                        markerID[numRecords] = recordI.getIDStr();
-//                        REF[numRecords]      = recordI.getRefStr();
-//                        ALT[numRecords]      = recordI.getAltStr();
-//                        SumDat[numRecords].resize(5,0);
-//                        // [0]sumX [1]sumY [2]sumXY [3]sumX2 [4]sumY2
-//
-//                        vector<double> &DosageV = TempRecordV[j].Dosage;
-//                        VcfRecordGenotype& GenotypeI = recordI.getGenotypeInfo();
-//                        vector<double> &temp = SumDat[numRecords];
-//
-//                        for (int i = 0; i < numSamples; i++)
-//                        {
-//                            double X = DosageV[i];
-//                            double Y = stod(*GenotypeI.getString("DS",i));
-//                            temp[0] += X; temp[1] += Y; temp[2] += X*Y; temp[3] += X*X; temp[4] += Y*Y;
-//                        }
-//
-//                        numRecords++;
-//
-//
-//                        break;
-//                    }
-//                }
-//
-//                if (!inFileI.readRecord(recordI)) { EndRecord = true; break;}
-//                chrI = chr2int(recordI.getChromStr()); posI = recordI.get1BasedPosition();
-//                if (posI != currentPos or chrI != currentChr) { break; }
-//                altI = recordI.getAltStr();
-//            }
-//
-//        }
-//
-//    }
-//
-//    cout << endl;
-//    cout << "Printing SumDat:" << endl;
-//    for(int i=0; i < numRecords; i++)
-//    {
-//        cout << i << " ";
-//        for (int j=0; j< 5; j++)
-//            cout << SumDat[i][j] << " ";
-//        cout << endl;
-//    }
 
 
     return false;
